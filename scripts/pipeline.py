1f02baf0dd90e29dfdc03a2e66d72860ea53135a
proudnotice <jasonliu0704@gmail.com>
2024-10-15 11:15:52 +0800

    ud

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -149,7 +149,7 @@ def chattts_infer(text:str, stream=True):
             data_size = 0
 
             # Create an iterator for the response content
-            chunks = response.iter_content(chunk_size=16384)            
+            chunks = response.iter_content(chunk_size=65536)            
             # Read the WAV header (44 bytes)
             while len(header) < 44:
                 try:
